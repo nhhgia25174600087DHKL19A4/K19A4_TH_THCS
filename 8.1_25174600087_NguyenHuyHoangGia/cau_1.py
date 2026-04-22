0aafbdccc8dcf947e784329e335af2b199a2f609
nhhgia25174600087DHKL19A4 <nhhgia.25174600087@sv.uneti.edu.vn>
2026-04-22 11:25:35 +0700

    Delete 8.1_25174600087_NguyenHuyHoangGia directory

--- 8.1_25174600087_NguyenHuyHoangGia/cau_1.py
+++
@@ -1,9 +0,0 @@
-def tinh_luy_thua():
-    co_so = int(input("Nhập cơ số (số tự nhiên): "))
-    so_mu = int(input("Nhập số mũ (số tự nhiên): "))
-    ket_qua = 1
-    for _ in range(so_mu):
-        ket_qua *= co_so
-        
-    print("Kết quả:", ket_qua)
-tinh_luy_thua()
