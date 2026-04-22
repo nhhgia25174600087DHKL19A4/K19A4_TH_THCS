660b4472b22c3fcb084027433db45d5637f2da2d
npd1008 <npduc.25174600028@sv.uneti.edu.vn>
2026-04-22 09:44:04 +0700

    8.1_25174600028_NguyenPhuongDuc

---
+++ 8.1_25174600028_NguyenPhuongDuc/bai_6.py
@@ -0,0 +1,7 @@
+def so_ke_tiep(n):
+    return n + 1
+
+def chuong_trinh_chinh():
+    n = int(input("nhập vào một số nguyên: "))
+    print("Số kế tiếp là:", so_ke_tiep(n))
+chuong_trinh_chinh()
---
+++ 8.1_25174600028_NguyenPhuongDuc/bai_9.py
@@ -0,0 +1,11 @@
+def phep_tinh_co_ban(a, b):
+    print(f"{a} + {b} = {a + b}")
+    print(f"{a} - {b} = {a - b}")
+    print(f"{a} * {b} = {a * b}")
+    if b != 0:
+        print(f"{a} / {b} = {a / b}")
+    else:
+        print("Không thể thực hiện phép chia cho 0.")
+
+def tinh_luy_thua_co_tham_so(x, y):
+    return x ** y 
